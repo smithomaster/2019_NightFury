2a55ebd303538497038813a183eeb23b78a110d9
Emil Smith <smithomaster@gmail.com>
2019-05-28 16:58:03 +0930

    stuff

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -26,17 +26,20 @@ void opcontrol() {
 		pros::vision_object_s_t rtn = vision_sensor.get_by_sig(0, TORTEX_YELLOW);
 
 		if (rtn.signature == 2){
-			while (rtn.x_middle_coord < 310) {
-				drive.rotate(-50);
+			if(rtn.x_middle_coord < 140){
+				drive.right(60);
+				drive.left(30);
 			}
-		
-			while(rtn.x_middle_coord > 330) {
-				drive.rotate(50);
+			if(rtn.x_middle_coord >170){
+				drive.left(60);
+				drive.right(30);
 			}
-
-			while (rtn.x_middle_coord > 310 && rtn.x_middle_coord <330 && rtn.width < 400){
-				drive.forward(50);
+			if(rtn.x_middle_coord > 140 && rtn.x_middle_coord < 170){
+				drive.forward (10);
 			}
+			// while (rtn.x_middle_coord > 310 && rtn.x_middle_coord <330 && rtn.width < 400){
+			// 	drive.forward(50);
+			// }
 		}
 		// Prints "sig: 1"
 		pros::delay(2);
