a928ac5d2fd36c3ccfcaaa4a5607e90d0b79a47f
Emil Smith <smithomaster@gmail.com>
2019-06-18 16:18:05 +0930

    labelling of globals sorting in logical order and tagging of global variables

--- src/globals.cpp
+++ src/globals.cpp
@@ -1,23 +1,30 @@
 #include "main.h"
 
-int FRONT_LEFT_MOTOR_PORT =  1;
-int BACK_LEFT_MOTOR_PORT =  2;
-int LEFT_LINEAR_MOTOR_PORT = 3;
-int RIGHT_LINEAR_MOTOR_PORT = 8;
-int FRONT_RIGHT_MOTOR_PORT =  9;
-int BACK_RIGHT_MOTOR_PORT =  10;
+// vision
 int VISION_PORT = 11;
 int TORTEX_YELLOW = 2;
+pros::Vision camera(VISION_PORT);
+
+// controllers
 okapi::Controller master;
 
-pros::Vision camera(VISION_PORT);
+// drive motors
+int FRONT_LEFT_MOTOR_PORT = 1;
+int BACK_LEFT_MOTOR_PORT = 2;
+
+// linear motors
+int LEFT_LINEAR_MOTOR_PORT = 3;
+int RIGHT_LINEAR_MOTOR_PORT = 8;
+int FRONT_RIGHT_MOTOR_PORT = 9;
+int BACK_RIGHT_MOTOR_PORT = 10;
+
+// chassis
 using namespace okapi::literals;
 okapi::ChassisControllerIntegrated drive = okapi::ChassisControllerFactory::create(
     {FRONT_LEFT_MOTOR_PORT, BACK_LEFT_MOTOR_PORT}, {FRONT_RIGHT_MOTOR_PORT, BACK_RIGHT_MOTOR_PORT},
     okapi::AbstractMotor::gearset::green,
     {4_in, 11.5_in}
 );
-
 okapi::ChassisControllerIntegrated linear = okapi::ChassisControllerFactory::create(
     LEFT_LINEAR_MOTOR_PORT, RIGHT_LINEAR_MOTOR_PORT, 
     okapi::AbstractMotor::gearset::red
--- src/globals.hpp
+++ src/globals.hpp
@@ -3,19 +3,26 @@
 #ifndef GLOBALS
 #define GLOBALS
 
+// vision
+extern int VISION_PORT;
+extern int TORTEX_YELLOW;
+extern pros::Vision camera;
+
+// controllers
+extern okapi::Controller master;
+
+// drive motors
 extern int FRONT_LEFT_MOTOR_PORT;
 extern int BACK_LEFT_MOTOR_PORT;
+
+// linear motors
 extern int LEFT_LINEAR_MOTOR_PORT;
 extern int RIGHT_LINEAR_MOTOR_PORT;
 extern int FRONT_RIGHT_MOTOR_PORT;
 extern int BACK_RIGHT_MOTOR_PORT;
-extern int VISION_PORT;
-extern int TORTEX_YELLOW;
 
-extern okapi::Controller master;
-extern pros::Vision camera;
+// chassis
 extern okapi::ChassisControllerIntegrated drive;
-
 extern okapi::ChassisControllerIntegrated linear;
 
 #endif
