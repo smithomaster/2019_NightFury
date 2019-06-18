14de83d1a0ad63ca0c6bca1eb42e028e34aacf8a
Emil Smith <smithomaster@gmail.com>
2019-06-18 16:09:32 +0930

    remove descriptive comment removal of the comments describing the function of the functions

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -1,19 +1,6 @@
 #include "main.h"
 #include "globals.hpp"
 
-/**
- * Runs the operator control code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the operator
- * control mode.
- *
- * If no competition control is connected, this function will run immediately
- * following initialize().
- *
- * If the robot is disabled or communications is lost, the
- * operator control task will be stopped. Re-enabling the robot will restart the
- * task, not resume it from where it left off.
- */
 void opcontrol() {
 	while(true){
 		if(master.getDigital(okapi::ControllerDigital::A) == true){
@@ -22,9 +9,7 @@ void opcontrol() {
 		}
 		drive.tank(master.getAnalog(okapi::ControllerAnalog::leftY),
 				   master.getAnalog(okapi::ControllerAnalog::rightY) * -1);
-
 		
-		// Prints "sig: 1"
 		pros::delay(2);
 	}
 }
